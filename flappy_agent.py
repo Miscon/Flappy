eb5b3f44dc04ace56feeaedeade194dba58a6b31
Athena <athenabjorg@gmail.com>
2018-10-18 18:08:25 +0000

    sorry broken code, need to map states

--- flappy_agent.py
+++ flappy_agent.py
@@ -1,10 +1,13 @@
 from ple.games.flappybird import FlappyBird
 from ple import PLE
 import random
+import numpy as np
 
 class FlappyAgent:
     def __init__(self):
-        # TODO: you may need to do some initialization for your agent here
+        self.Q = {} # see picture or rl2, averaging learning rule
+        self.greedy_policy = {}
+        self.episode = []
         return
     
     def reward_values(self):
@@ -26,8 +29,55 @@ class FlappyAgent:
             subsequent steps in the same episode. That is, s1 in the second call will be s2
             from the first call.
             """
+        # when we learn with on policy monte carlo we need the whole episode and then we can update
         # TODO: learn from the observation
+        # 1. remember these statetransitions = episode.append((s1, a, r))
+
+        # TODO turn state into set first
+        s1 = (s1["player_y"], s1["player_vel"], s1["next_pipe_dist_to_player"], s1["next_pipe_bottom_y"])
+        self.episode.append((s1, a, r))
+
+        # 2. if reached the end of episode, learn from it, then reset = episode = [] part(b)
+        if end:
+            self.learn_from_episode(self.episode)
+            self.episode = []
+
         return
+
+    def learn_from_episode(self, episode):        
+        #instead of append G to returns, use the update rule # check photo on phone"
+        a = 0.1 # TODO learning rate, set correctly
+        gamma = 0.9 # TODO discount, set correctly
+        states = set()
+        
+        # b)
+        G = 0
+        for s, a, r in reversed(episode):
+            print(type(s))
+            G = r + gamma * G
+            self.Q[(s, a)] = self.Q[(s, a)] + a * (G - self.Q[(s, a)]) # update rule
+            states.add(s)
+        
+        # c)
+        for s in states: 
+            A_star = None
+            ret = None
+            # A more generalized loop over actions even though they are just two
+            for action in [0, 1]:
+                q_value = self.Q.get((s, action)) # get returns None if none exists
+                if q_value is not None:
+                    if ret is None:
+                        A_star = action
+                        ret = q_value
+                    else:
+                        if ret < q_value:
+                            A_star = action
+                            ret = q_value
+            if A_star is not None:
+                self.greedy_policy[s] = A_star
+
+
+        
 
     def training_policy(self, state):
         """ Returns the index of the action that should be done in state while training the agent.
@@ -35,10 +85,25 @@ class FlappyAgent:
 
             training_policy is called once per frame in the game while training
         """
+
+
         print("state: %s" % state)
         # TODO: change this to to policy the agent is supposed to use while training
         # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1) 
+
+        # with epsilon chance, pick a random action
+        # else the policy greedy action
+        # only initialize states when you see them, to 0
+
+        e = 0.1
+        greedy = np.random.choice([False, True], p=[e, 1-e])
+
+        action = self.greedy_policy.get(state) # get returns None if none exists
+        if greedy or action is None:
+            action = random.randint(0, 1)
+
+        return action
+
 
     def policy(self, state):
         """ Returns the index of the action that should be done in state when training is completed.
@@ -50,37 +115,64 @@ class FlappyAgent:
         print("state: %s" % state)
         # TODO: change this to to policy the agent has learned
         # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1) 
+        action = self.greedy_policy.get(state) # get returns None if none exists
+        if action is None:
+            action = random.randint(0, 1)
+
+        return action
 
 def run_game(nb_episodes, agent):
     """ Runs nb_episodes episodes of the game with agent picking the moves.
         An episode of FlappyBird ends with the bird crashing into a pipe or going off screen.
     """
 
-    reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
-    # TODO: when training use the following instead:
-    # reward_values = agent.reward_values
-    
-    env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=False, rng=None,
-            reward_values = reward_values)
-    # TODO: to speed up training change parameters of PLE as follows:
-    # display_screen=False, force_fps=True 
-    env.init()
+    training = True
 
-    score = 0
-    while nb_episodes > 0:
-        # pick an action
-        # TODO: for training using agent.training_policy instead
-        action = agent.policy(env.game.getGameState())
-
-        # step the environment
-        reward = env.act(env.getActionSet()[action])
-        print("reward=%d" % reward)
-
-        # TODO: for training let the agent observe the current state transition
-
-        score += reward
+    if training:
+        reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
+        # reward_values = agent.reward_values
         
+        env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=False, rng=None,
+                reward_values = reward_values)
+        # TODO: to speed up training change parameters of PLE as follows:
+        # display_screen=False, force_fps=True 
+        env.init()
+
+        score = 0
+        while nb_episodes > 0:
+            # pick an action
+            state1 = env.game.getGameState()
+            action = agent.training_policy(state1)
+
+            # step the environment
+            reward = env.act(env.getActionSet()[action])
+            print("reward=%d" % reward)
+
+            state2 = env.game.getGameState()
+
+            agent.observe(state1, action, reward, state2, env.game_over())
+
+            score += reward
+        
+        else:
+            reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
+            
+            env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=False, rng=None,
+                    reward_values = reward_values)
+            env.init()
+
+            score = 0
+            while nb_episodes > 0:
+                # pick an action
+                action = agent.policy(state1)
+
+                # step the environment
+                reward = env.act(env.getActionSet()[action])
+                print("reward=%d" % reward)
+
+                score += reward
+
+
         # reset the environment if the game is over
         if env.game_over():
             print("score for this episode: %d" % score)
